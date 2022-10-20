0dfec8c6b94bc0396829f7008d4fc6022550528d
RyoDeyn <jaimebienlepain@gmail.com>
2022-10-21 00:27:25 +0200

    Partie affichage terminée.

---
+++ pureforce.py
@@ -0,0 +1,19 @@
+# On affiche le message de bienvenue :
+print("\n=========== Welcome on Pure-Force ! ============\n"
+      "====== A free and open password generator. =======\n"
+      "--------------------------------------------------\n"
+      "Did you know that 'pain' means 'bread' in french ?")
+
+# On affiche la forme d'utilisation de pureforce :
+print("Usage : pureforce [COMMAND]\n")
+
+# On affiche les commandes possibles :
+print("Possible commands :\n"
+      "-b (or --basic)              starts the basic mode, which generate an exhaustive list\n"
+      "                             of passwords from a given range. Use basic interactive questions\n"
+      "                             to modulate the range. It is the simplest mode but doesn't use\n"
+      "                             any optimization.\n"
+      "-i (or --intelligentia)      starts the intelligent mode, which generate an optimized list of\n"
+      "                             passwords. It will use more interactive questions to select the\n"
+      "                             most relevant passwords.\n"
+      "-h (or --help)               shows the possible commands (this menu).\n")
