dd8cb495210435c8f942cf854950ab2efbd6fab0
RyoDeyn <jaimebienlepain@gmail.com>
2022-10-25 12:22:14 +0200

    Derniers cas d'options et paramètres possibles gérés.

--- pureforce.py
+++ pureforce.py
@@ -17,6 +17,7 @@ __copyright__ = "Copyright (C) 2022 Alexandre Quéré"
 import sys  # sert à prendre les arguments en ligne de commande
 import getopt  # sert à parser les arguments en ligne de commande
 
+
 def welcome_message():
     """Affiche un message de bienvenue.
     Affiche également des explications sur l'utilisation du script.
@@ -41,6 +42,7 @@ def welcome_message():
           "                             most relevant passwords.\n"
           "-h (or --help)               shows the possible options (this menu).\n")
 
+
 def process_options():
     """Gère les options et les paramètres du script."""
     # couples : liste de couples [options, valeur] ; exemple : (-l, 4);(-p, "er")
@@ -48,7 +50,7 @@ def process_options():
     # # Ici nos options ne prennent pas d'arguments donc on ne s'occupe pas de 'valeur'.
     # # On ne s'occupera pas non plus de argsAfter.
     try:
-        couples, args_after = getopt.getopt(sys.argv[1:], "bih", ["basic", "intelligentia", "help"]);
+        couples, args_after = getopt.getopt(sys.argv[1:], "bih", ["basic", "intelligentia", "help"])
     except getopt.GetoptError as error:
         # On affiche un message d'erreur :
         print("\nError : ", error)
@@ -67,7 +69,7 @@ def process_options():
         print("\nError : pureforce do not accept several options at the same time.\n"
               "Example of correct use : python pureforce -b\n"
               "Example of incorrect use : python pureforce -b -i\n")
-    # Cas où la syntaxe est correcte :
+    # Cas où l'usage est correcte :
     else:
         for option, value in couples:
             if option in ("-h", "--help"):
@@ -77,9 +79,11 @@ def process_options():
             elif option in ("-i", "--intelligentia"):
                 print("you chose the intelligentia command !")
 
+
 def main():
     """Fonction principale."""
     process_options()
+
 
 if __name__ == '__main__':
     main()
