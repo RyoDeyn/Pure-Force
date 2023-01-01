3b2837f1d78ef1d3a6e0964814c4db1f7eb299bd
Ryo <99072427+RyoDeyn@users.noreply.github.com>
2023-01-01 17:15:14 +0100

    Correction test fichier déjà présent.

--- pureforce.py
+++ pureforce.py
@@ -295,7 +295,7 @@ def basic_message():
     writing_mode = 'w'
 
     # Si le fichier existe déjà :
-    if os.path.isfile(f"{os.path.abspath(os.getcwd())}\\{file_name}"):
+    if os.path.isfile(f"{os.path.abspath(os.getcwd())}/{file_name}"):
         print(f"{TColor.yellow}Warning : the file {file_name} already exists. Choose the option you want :"
               f"\n- create a new file (by changing the name) (1)"
               f"\n- override the existing file (2)"
